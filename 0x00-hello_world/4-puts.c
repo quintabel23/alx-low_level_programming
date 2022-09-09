0c3469aa82028a6d2df72571243d3fd6c47bb8ad
quintabel23 <chikwendugrace23@gmail.com>
2022-09-08 18:17:04 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#include <stdio .h>
+#include <stdio.h>
 
 /**
- * main - Printing
- *
- * Return: Always 0 (Sucessful)
- */
-
+* main - Printing
+*
+* Return: Always 0 (Sucessful)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
