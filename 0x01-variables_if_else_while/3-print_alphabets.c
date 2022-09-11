387a41e33458ea361289e7592e0eae56cc07387a
quintabel23 <chikwendugrace23@gmail.com>
2022-09-11 15:44:56 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-	
+
 	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
-	
+
 	putchar('\n');
 
 	return (0);
