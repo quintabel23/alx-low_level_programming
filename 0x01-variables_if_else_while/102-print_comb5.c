15b99991ab9d0e1c6f55d3952c48b026b38d5437
quintabel23 <chikwendugrace23@gmail.com>
2022-09-12 17:45:31 -0700

    102 print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 	for (i = 0; i < 100; i++)
 	{
 
-		for (p = 0; p <100; i++)
+		for (p = 0; p < 100; i++)
 		{
 
 			if (p > i)
@@ -23,7 +23,7 @@ int main(void)
 				putchar(' ');
 				putchar((p / 10) + '0');
 				putchar((p % 10) + '0');
-				if (i !=98)
+				if (i != 98)
 				{
 
 					putchar(',');
