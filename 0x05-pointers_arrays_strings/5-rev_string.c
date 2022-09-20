4be4d34c7ff9e70bc2c46720591fa06e95de6ca1
quintabel23 <chikwendugrace23@gmail.com>
2022-09-19 21:17:09 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,23 +8,22 @@
 
 void rev_string(char *s)
 {
-	"sam"->"mas"
-	char tmp;
-	int i, lenght1, lenght2;
+	int i, j, k, tmp;
 
-	lenght1 = 0;
-	lenght2 = 0;
-
-	while (s[lenght1] != '\0')
-		lenght1++
-
-	lenght2 = lenght1 - 1
-	for (i = 0; i < lenght1 / 2; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		"samalx" - "xlamas"
-		tmp = s[i];
-		s[i] = s[lenght2];
-		s[lenght2] = tmp;
-		lenght2 = 1;
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		tmp = s[k];
+		s[k] = s[j];
+		s[j] = tmp;
+		k++;
+		j--;
 	}
 }
