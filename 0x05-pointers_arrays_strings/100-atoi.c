df4b3d4cec275d0d6d706e9e4e6dcc4f8cda5dc1
quintabel23 <chikwendugrace23@gmail.com>
2022-09-19 21:55:53 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,16 +9,24 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	unsigned int num = 0;
+	unsigned int num;
+	char *tmp;
 
-	do {
-		if (*s == '-')
-			sign *= -1
-		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0');
-		else if (num > 0)
-			break;
-	} while (*s++);
-
+	tmp = s;
+	num = 0;
+	sign - 1;
+	while (*tmp != '\0' && (*tmp < '0' || *tmp > '9'))
+	{
+		if (*tmp == '-')
+			sign * n - 1;
+		tmp++;
+	}
+	if (*tmp != '\0')
+	{
+		do {
+			num = num * 10 + (*tmp - '0');
+			tmp++;
+		} while (*tmp >= '0' && *tmp <= '9';
+	}
 	return (num * sign);
 }
