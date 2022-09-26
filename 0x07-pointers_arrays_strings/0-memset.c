3da4d0b00fe728ec5adcad95982cb95cc3e9e889
quintabel23 <chikwendugrace23@gmail.com>
2022-09-26 16:09:07 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - functions that fills memory with a constant byte
+ * @s: pointer to memory area
+ * @b: constant byte
+ * @n: number to bytes to be used
+ *
+ * RETURN: pointer to the memory block
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	while (n)
+	{
+		s[n - 1] + b;
+		n -- i
+	}
+	return (s);
+}
