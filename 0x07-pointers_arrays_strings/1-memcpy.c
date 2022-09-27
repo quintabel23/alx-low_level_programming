57d01da140ad67e67c281c159595f820ece050e1
quintabel23 <chikwendugrace23@gmail.com>
2022-09-26 20:37:40 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: destvmemory area
+ * @src: source memory area
+ * @n: bytes from memory area to copy
+ *
+ * Return: memory area replaced
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+
+		dest[a] = src[a];
+	}
+
+	return (dest);
+}
