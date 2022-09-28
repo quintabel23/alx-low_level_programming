82875feebfd69624268bb903d8e5d4a679009848
quintabel23 <chikwendugrace23@gmail.com>
2022-09-28 16:01:16 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _pow_recursion - return the value of x to the power of y
- * @x: number
- * @y: power
+ * @x: number.
+ * @y: power.
  *
  * Return: x to the pow of y
  * -1 if x < 0
@@ -14,12 +14,9 @@ int _pow_recursion(int x, int y);
 
 	if (y < 0)
 		return (-1);
-
 	if (y == 0)
 		return (1);
-
 	if (y == 1)
 		return (x);
-
 	return (x * _pow_recursion(x, y - 1));
 }
