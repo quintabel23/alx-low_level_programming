7f41e790fb519da987ec80f99b4411120063f1a6
quintabel23 <chikwendugrace23@gmail.com>
2022-09-28 13:38:29 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,16 +3,20 @@
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: The string to be printed
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
 
-	if (*s)
+	if (*s == "\0")
 	{
-		_putchar(*s)
-		_puts_recursion(s + 1);
+		_putchar("\n");
 	}
 
 	else
-		_putchar("\n")
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
