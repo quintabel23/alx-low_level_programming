3fa42502321b58999ce325a64d369137740334cf
quintabel23 <chikwendugrace23@gmail.com>
2022-10-03 07:37:08 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "stdlib.h"
+#include <stdlib.h>
 
 /**
  * create _array - create array
@@ -8,21 +8,25 @@
  * Return: pointer to the array or NULL if it fails
  */
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 
 	char *array;
 	unsigned int i;
 
 	if (size == 0)
+	{
 		return (NULL);
+	}
 	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
+	{
 		array[i] = c;
+	}
 
 	return (array);
 }
