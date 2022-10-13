53dae0cc2b2d6e731267a549297479e8c7f09cbc
quintabel23 <chikwendugrace23@gmail.com>
2022-10-12 19:36:41 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,6 +19,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		print("Error\n");
+		exit(99);
+	}
 
 	s = argv[2][0];
 	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
