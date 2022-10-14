168189c978fe9a8d1241b194e448a7c4b3d53bcb
quintabel23 <chikwendugrace23@gmail.com>
2022-10-13 21:27:44 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -26,13 +26,13 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-			printf("%s%c", separator, va_arg(args, int));
+				printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
-			printf("%s%d", separator, va_arg(args, int));
+				printf("%s%d", separator, va_arg(args, int));
 				break;
 			case 'f':
-			printf("%s%f", separator, va_arg(args, double));
+				printf("%s%f", separator, va_arg(args, double));
 				break;
 			case 's':
 				s = va_arg(args, char *);
@@ -42,6 +42,7 @@ void print_all(const char * const format, ...)
 				break;
 			default:
 				i++;
+				continue;
 		}
 		separator = ", ";
 		i++;
