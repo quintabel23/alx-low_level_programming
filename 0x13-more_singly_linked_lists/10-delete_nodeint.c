5277711930d5452fc039d540f7fe7f508f8ddac7
quintabel23 <chikwendugrace23@gmail.com>
2022-10-25 18:28:26 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,6 +14,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *copy = *head;
 	unsigned int node;
 
+	if (copy == NULL)
+		return (-1);
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
