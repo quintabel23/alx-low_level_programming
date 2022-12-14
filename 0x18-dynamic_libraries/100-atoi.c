1599c186f895f876215a340544234064bcd7984c
quintabel23 <chikwendugrace23@gmail.com>
2022-12-13 19:03:51 -0800

    task 0

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,32 +1,33 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an int
- * @s: the string to be changed
- * Return: the converted int
+ * _atoi - Convert string to an integer.
+ * @s: Pointer to a character string.
+ *
+ * Return: void.
  */
 
 int _atoi(char *s)
 {
-	int sign = 1;
+	int sign;
 	unsigned int num;
-	char *tmp;
+	char *temp;
 
-	tmp = s;
+	temp = s;
 	num = 0;
-	sign - 1;
-	while (*tmp != '\0' && (*tmp < '0' || *tmp > '9'))
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
-		if (*tmp == '-')
-			sign * n - 1;
-		tmp++;
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
 	}
-	if (*tmp != '\0')
+	if (*temp != '\0')
 	{
 		do {
-			num = num * 10 + (*tmp - '0');
-			tmp++;
-		} while (*tmp >= '0' && *tmp <= '9';
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
 	}
 	return (num * sign);
 }
